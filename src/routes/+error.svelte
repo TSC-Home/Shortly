d2e1d183b370be5911e6fad3a0c607cdb961b90b
ZERO <103266275+TSC-Home@users.noreply.github.com>
2023-11-04 15:05:31 +0100

    add title

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
     import { page } from '$app/stores';
 </script>
+<title>Shortly | {$page.status} </title>
 <div class="flex justify-center items-center" >
 <h1>{$page.status}: {$page.error?.message}</h1>
 </div>
